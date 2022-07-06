b7eb576a22168c88fe70eb25c7614fc177cc2d19
Baerdon <jardabazant@seznam.cz>
2022-07-06 15:16:14 +0200

    Added new class

---
+++ progitforprogrammers/ProGitForProgrammers/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ProGitForProgrammers
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public Book()
+        {
+        }
+    }
+}
+
